4aa5718eba5e994aa8580a2d6efa4d4d4c195fda
TetrisIQ <a@b.com>
2024-03-15 12:29:40 +0100

    Adding Schema TeAttributesV2 Version 2

---
+++ src/main/java/de/bonprix/trinity/shoplibrary/gen/TeAttributesV2.java
@@ -0,0 +1,88 @@
+package de.bonprix.trinity.shoplibrary.gen;
+
+import de.bonprix.trinity.shoplibrary.AbstractAttributes;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.bonprix.trinity.shoplibrary.Schema;
+import lombok.Builder;
+import lombok.ToString;
+import lombok.Getter;
+import java.util.List;
+import java.util.Arrays;
+import de.bonprix.trinity.shoplibrary.datatype.*;
+import de.bonprix.trinity.shoplibrary.exception.FieldIsMandatoryException;
+import de.bonprix.trinity.shoplibrary.exception.UnsuportedValueException;
+import de.bonprix.trinity.shoplibrary.exception.SupportedValuesException;
+
+
+
+/**
+ * THIS CODE IS AUTO GENERATED <br>
+ * BY TRINITY TOOLS CODE GENERATION <br>
+ * DEVELOPED BY BonPrix <br> <br>
+ * This Class uses a Builder Pattern - use TeAttributesV2.builder() to create a Builder
+ * the builder evaluates the Attributes against the Schema and throws <b>RuntimeExceptions</b>
+ */
+
+@Getter
+@Builder
+@ToString
+public class TeAttributesV2 extends AbstractAttributes {
+
+	@JsonIgnore
+	private transient final Schema schema = new Schema("TE", 2L);
+
+	// Attributes
+	/**
+	 * 
+	*/
+	@JsonProperty("aa")
+	private Double aa;
+	/**
+	 * 
+	*/
+	@JsonProperty("asd")
+	private Double asd;
+
+	@Override
+	public void encryptValues(int clientId) {
+
+	}
+
+    @Override
+    public boolean validate(Schema schema) {
+        return schema.equals(this.schema);
+    }
+
+    // Uses custom builder class
+    /**
+     * <br>
+     * aa is not Mandatory  <br>
+     * asd is not Mandatory  <br>
+	*/
+	public static TeAttributesV2Builder builder() {
+		return new TeAttributesV2.CustomAttributesBuilder();
+	}
+
+	/**
+	 * Custom  builder class
+	 */
+	private static class CustomAttributesBuilder extends TeAttributesV2Builder {
+
+		@Override
+		public TeAttributesV2 build() {
+			// Validates required fields
+
+			return super.build();
+		}
+
+	    private static <T> boolean compareWithList(T value, List<T> supportedValues) {
+	        for (T supportedValue : supportedValues) {
+	            if (value.equals(supportedValue)) {
+			        return true;
+			    }
+	        } return false;
+		}
+	}
+
+}
