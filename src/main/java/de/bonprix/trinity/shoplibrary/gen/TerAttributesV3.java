6c36bb8868ebb6cd60e5ac0b1b032ce3614fb63c
TetrisIQ <a@b.com>
2024-03-15 15:47:27 +0100

    Adding Schema TerAttributesV3 Version 3

---
+++ src/main/java/de/bonprix/trinity/shoplibrary/gen/TerAttributesV3.java
@@ -0,0 +1,95 @@
+package de.bonprix.trinity.shoplibrary.gen;
+
+import de.bonprix.trinity.shoplibrary.AbstractAttributes;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.bonprix.trinity.shoplibrary.Schema;
+import lombok.Builder;
+import lombok.ToString;
+import lombok.Getter;
+import java.util.List;
+import java.util.Arrays;
+import de.bonprix.trinity.shoplibrary.datatype.*;
+import de.bonprix.trinity.shoplibrary.exception.FieldIsMandatoryException;
+import de.bonprix.trinity.shoplibrary.exception.UnsuportedValueException;
+import de.bonprix.trinity.shoplibrary.exception.SupportedValuesException;
+
+
+
+/**
+ * THIS CODE IS AUTO GENERATED <br>
+ * BY TRINITY TOOLS CODE GENERATION <br>
+ * DEVELOPED BY BonPrix <br> <br>
+ * This Class uses a Builder Pattern - use TerAttributesV3.builder() to create a Builder
+ * the builder evaluates the Attributes against the Schema and throws <b>RuntimeExceptions</b>
+ */
+
+@Getter
+@Builder
+@ToString
+public class TerAttributesV3 extends AbstractAttributes {
+
+	@JsonIgnore
+	private transient final Schema schema = new Schema("TER", 3L);
+
+	// Attributes
+   /**
+	 * 
+   */
+	@JsonProperty("etr")
+	private String etr;
+	/**
+	 * 
+	*/
+	@JsonProperty("ret")
+	private Long ret;
+	/**
+	 * 
+	*/
+	@JsonProperty("ter")
+	private Long ter;
+
+	@Override
+	public void encryptValues(int clientId) {
+
+	}
+
+    @Override
+    public boolean validate(Schema schema) {
+        return schema.equals(this.schema);
+    }
+
+    // Uses custom builder class
+    /**
+     * ter is Mandatory  <br>
+     * <br>
+     * etr is not Mandatory  <br>
+     * ret is not Mandatory  <br>
+	*/
+	public static TerAttributesV3Builder builder() {
+		return new TerAttributesV3.CustomAttributesBuilder();
+	}
+
+	/**
+	 * Custom  builder class
+	 */
+	private static class CustomAttributesBuilder extends TerAttributesV3Builder {
+
+		@Override
+		public TerAttributesV3 build() {
+			// Validates required fields
+			if(super.ter == null) throw new FieldIsMandatoryException("ter is Mandatory");
+
+			return super.build();
+		}
+
+	    private static <T> boolean compareWithList(T value, List<T> supportedValues) {
+	        for (T supportedValue : supportedValues) {
+	            if (value.equals(supportedValue)) {
+			        return true;
+			    }
+	        } return false;
+		}
+	}
+
+}
